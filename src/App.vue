e0657f76f2e153992907aff6cc00a8aa681cd35e
Mikołaj Lechtański <mikolec@gmail.com>
2020-09-14 09:41:13 +0200

    created project 'Build a Reading List Application With Vue.js'

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BookList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BookList from "./components/BookList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    BookList,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/BookList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul>
+      <BookItem v-for="book in books" :key="book.title" :book="book" />
+      <BookForm @addBook="appendBook" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import BookItem from "@/components/BookItem.vue";
+import BookForm from "@/components/BookForm.vue";
+
+export default {
+  name: "BookList",
+  data() {
+    return {
+      title: "All Books",
+      books: [
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+        { title: "American Gods", author: "Neil Gaiman" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman" },
+        {
+          title: "Daring Greatly: How the courage to be vulnerable transforms the way we live, love, parent and lead",
+          author: "Brene Brown",
+        },
+        { title: "Don't make me think", author: "Steve Krug" },
+        { title: "DRiVE", author: "Daniel Pink" },
+      ],
+    };
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor });
+    },
+  },
+  components: { BookItem, BookForm },
+};
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+</style>
